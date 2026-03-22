656d528638eeda339f5ba82f3d9a9835eda73237
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:47:43 +0200

    upgrade lesson quality: 01-Easy-Fundamentals/06-Functions-Basics

--- 01-Easy-Fundamentals/06-Functions-Basics/06-Functions-Basics.test.js
+++ 01-Easy-Fundamentals/06-Functions-Basics/06-Functions-Basics.test.js
@@ -20,11 +20,25 @@ describe('Functions-Basics', () => {
   });
 
   test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
-    expect(solveFunctionsBasics([NaN, -0, '0'])).toEqual({
+    expect(solveFunctionsBasics([NaN, -0, "0"])).toEqual({
       topic: 'Functions-Basics',
       total: 3,
       truthyCount: 1,
       falsyCount: 2,
     });
+  });
+
+  test('Validation: throws when input is not an array', () => {
+    expect(() => solveFunctionsBasics(null)).toThrow('values must be an array');
+    expect(() => solveFunctionsBasics('not-an-array')).toThrow('values must be an array');
+  });
+
+  test('Safety: does not mutate the input array', () => {
+    const input = [1, 0, 'x'];
+    const clone = [...input];
+
+    solveFunctionsBasics(input);
+
+    expect(input).toEqual(clone);
   });
 });
