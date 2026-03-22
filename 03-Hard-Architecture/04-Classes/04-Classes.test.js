5684754c2535ebfa96d53ee82a8bfcdd48729f65
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:48:23 +0200

    upgrade lesson quality: 03-Hard-Architecture/04-Classes

--- 03-Hard-Architecture/04-Classes/04-Classes.test.js
+++ 03-Hard-Architecture/04-Classes/04-Classes.test.js
@@ -20,11 +20,25 @@ describe('Classes', () => {
   });
 
   test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
-    expect(solveClasses([NaN, -0, '0'])).toEqual({
+    expect(solveClasses([NaN, -0, "0"])).toEqual({
       topic: 'Classes',
       total: 3,
       truthyCount: 1,
       falsyCount: 2,
     });
+  });
+
+  test('Validation: throws when input is not an array', () => {
+    expect(() => solveClasses(null)).toThrow('values must be an array');
+    expect(() => solveClasses('not-an-array')).toThrow('values must be an array');
+  });
+
+  test('Safety: does not mutate the input array', () => {
+    const input = [1, 0, 'x'];
+    const clone = [...input];
+
+    solveClasses(input);
+
+    expect(input).toEqual(clone);
   });
 });
