fafed23bb250c8a5b3638e045f68cd38a7b5aafb
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 04:34:30 +0200

    Course build: 01-Easy-Fundamentals/01-Variables/03-const.js

--- 01-Easy-Fundamentals/01-Variables/03-const.js
+++ 01-Easy-Fundamentals/01-Variables/03-const.js
@@ -1,1 +1,30 @@
-module.exports = {};
+/**
+ * Analyze values for the const lesson.
+ *
+ * @param {unknown[]} values - List of values to inspect.
+ * @returns {{ topic: string, total: number, truthyCount: number, falsyCount: number }}
+ * Summary object for this lesson challenge.
+ * @throws {TypeError} When values is not an array.
+ */
+function solveConst(values) {
+  if (!Array.isArray(values)) {
+    throw new TypeError('values must be an array');
+  }
+
+  let truthyCount = 0;
+
+  for (const value of values) {
+    if (Boolean(value)) {
+      truthyCount += 1;
+    }
+  }
+
+  return {
+    topic: 'const',
+    total: values.length,
+    truthyCount,
+    falsyCount: values.length - truthyCount,
+  };
+}
+
+module.exports = solveConst;
