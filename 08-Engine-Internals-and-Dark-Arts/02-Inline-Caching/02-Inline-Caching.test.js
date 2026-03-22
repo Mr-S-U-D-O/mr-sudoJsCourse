d54d22d135239cadd46312ffdd95c7f0fc7c6af7
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:50:07 +0200

    upgrade lesson quality: 08-Engine-Internals-and-Dark-Arts/02-Inline-Caching

--- 08-Engine-Internals-and-Dark-Arts/02-Inline-Caching/02-Inline-Caching.test.js
+++ 08-Engine-Internals-and-Dark-Arts/02-Inline-Caching/02-Inline-Caching.test.js
@@ -20,11 +20,25 @@ describe('Inline-Caching', () => {
   });
 
   test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
-    expect(solveInlineCaching([NaN, -0, '0'])).toEqual({
+    expect(solveInlineCaching([NaN, -0, "0"])).toEqual({
       topic: 'Inline-Caching',
       total: 3,
       truthyCount: 1,
       falsyCount: 2,
     });
+  });
+
+  test('Validation: throws when input is not an array', () => {
+    expect(() => solveInlineCaching(null)).toThrow('values must be an array');
+    expect(() => solveInlineCaching('not-an-array')).toThrow('values must be an array');
+  });
+
+  test('Safety: does not mutate the input array', () => {
+    const input = [1, 0, 'x'];
+    const clone = [...input];
+
+    solveInlineCaching(input);
+
+    expect(input).toEqual(clone);
   });
 });
