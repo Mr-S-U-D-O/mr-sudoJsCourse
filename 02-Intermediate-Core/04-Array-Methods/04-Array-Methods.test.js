045a4483540b2dfa16b80458cb05a435bacb981a
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:47:59 +0200

    upgrade lesson quality: 02-Intermediate-Core/04-Array-Methods

--- 02-Intermediate-Core/04-Array-Methods/04-Array-Methods.test.js
+++ 02-Intermediate-Core/04-Array-Methods/04-Array-Methods.test.js
@@ -20,11 +20,25 @@ describe('Array-Methods', () => {
   });
 
   test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
-    expect(solveArrayMethods([NaN, -0, '0'])).toEqual({
+    expect(solveArrayMethods([NaN, -0, "0"])).toEqual({
       topic: 'Array-Methods',
       total: 3,
       truthyCount: 1,
       falsyCount: 2,
     });
+  });
+
+  test('Validation: throws when input is not an array', () => {
+    expect(() => solveArrayMethods(null)).toThrow('values must be an array');
+    expect(() => solveArrayMethods('not-an-array')).toThrow('values must be an array');
+  });
+
+  test('Safety: does not mutate the input array', () => {
+    const input = [1, 0, 'x'];
+    const clone = [...input];
+
+    solveArrayMethods(input);
+
+    expect(input).toEqual(clone);
   });
 });
