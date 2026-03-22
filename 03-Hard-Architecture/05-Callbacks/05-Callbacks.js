c0eba9aaea50614c80ca1d93a2b400fcf061c852
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 04:34:39 +0200

    Course build: 03-Hard-Architecture/05-Callbacks/05-Callbacks.js

--- 03-Hard-Architecture/05-Callbacks/05-Callbacks.js
+++ 03-Hard-Architecture/05-Callbacks/05-Callbacks.js
@@ -7,7 +7,24 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveCallbacks(values) {
-  // TODO: Your code here
+  if (!Array.isArray(values)) {
+    throw new TypeError('values must be an array');
+  }
+
+  let truthyCount = 0;
+
+  for (const value of values) {
+    if (Boolean(value)) {
+      truthyCount += 1;
+    }
+  }
+
+  return {
+    topic: 'Callbacks',
+    total: values.length,
+    truthyCount,
+    falsyCount: values.length - truthyCount,
+  };
 }
 
 module.exports = solveCallbacks;
