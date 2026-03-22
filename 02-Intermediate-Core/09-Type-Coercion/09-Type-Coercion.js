b902b80f1c05bebdf87a4c966028ee1097fb7dd0
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 04:34:35 +0200

    Course build: 02-Intermediate-Core/09-Type-Coercion/09-Type-Coercion.js

--- 02-Intermediate-Core/09-Type-Coercion/09-Type-Coercion.js
+++ 02-Intermediate-Core/09-Type-Coercion/09-Type-Coercion.js
@@ -7,7 +7,24 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveTypeCoercion(values) {
-  // TODO: Your code here
+  if (!Array.isArray(values)) {
+    throw new TypeError('values must be an array');
+  }
+
+  let truthyCount = 0;
+
+  for (const value of values) {
+    if (Boolean(value)) {
+      truthyCount += 1;
+    }
+  }
+
+  return {
+    topic: 'Type-Coercion',
+    total: values.length,
+    truthyCount,
+    falsyCount: values.length - truthyCount,
+  };
 }
 
 module.exports = solveTypeCoercion;
