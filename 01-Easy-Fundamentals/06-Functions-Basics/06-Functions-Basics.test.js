2e5cb4597efb39be62d28e44af532cbddd9624c1
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:31:18 +0200

    Add exercise scaffolding for 01-Easy-Fundamentals/06-Functions-Basics

---
+++ 01-Easy-Fundamentals/06-Functions-Basics/06-Functions-Basics.test.js
@@ -0,0 +1,30 @@
+const solveFunctionsBasics = require('./06-Functions-Basics.js');
+
+describe('Functions-Basics', () => {
+  test('Happy Path: counts truthy and falsy values', () => {
+    expect(solveFunctionsBasics([1, 0, 'JS', '', true, false])).toEqual({
+      topic: 'Functions-Basics',
+      total: 6,
+      truthyCount: 3,
+      falsyCount: 3,
+    });
+  });
+
+  test('Edge Case: handles an empty input array', () => {
+    expect(solveFunctionsBasics([])).toEqual({
+      topic: 'Functions-Basics',
+      total: 0,
+      truthyCount: 0,
+      falsyCount: 0,
+    });
+  });
+
+  test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
+    expect(solveFunctionsBasics([NaN, -0, '0'])).toEqual({
+      topic: 'Functions-Basics',
+      total: 3,
+      truthyCount: 1,
+      falsyCount: 2,
+    });
+  });
+});
