14e661cb343047d4c90361b5e3957a0ca0fd9a7e
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 04:34:34 +0200

    Course build: 02-Intermediate-Core/01-Scope-and-Hoisting/01-Scope-and-Hoisting.js

--- 02-Intermediate-Core/01-Scope-and-Hoisting/01-Scope-and-Hoisting.js
+++ 02-Intermediate-Core/01-Scope-and-Hoisting/01-Scope-and-Hoisting.js
@@ -7,7 +7,24 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveScopeAndHoisting(values) {
-  // TODO: Your code here
+  if (!Array.isArray(values)) {
+    throw new TypeError('values must be an array');
+  }
+
+  let truthyCount = 0;
+
+  for (const value of values) {
+    if (Boolean(value)) {
+      truthyCount += 1;
+    }
+  }
+
+  return {
+    topic: 'Scope-and-Hoisting',
+    total: values.length,
+    truthyCount,
+    falsyCount: values.length - truthyCount,
+  };
 }
 
 module.exports = solveScopeAndHoisting;
