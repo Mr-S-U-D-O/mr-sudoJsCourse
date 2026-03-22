2ae39fcd3f8a6091930a0adfd212547e0a83e796
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:31:19 +0200

    Add exercise scaffolding for 02-Intermediate-Core/02-Arrow-Functions

---
+++ 02-Intermediate-Core/02-Arrow-Functions/02-Arrow-Functions.js
@@ -0,0 +1,12 @@
+/**
+ * Analyze a list of values for the Arrow-Functions exercise.
+ *
+ * @param {unknown[]} values - Values to evaluate.
+ * @returns {{ topic: string, total: number, truthyCount: number, falsyCount: number }}
+ * An object containing the topic label and truthy/falsy counts.
+ */
+function solveArrowFunctions(values) {
+  // TODO: Your code here
+}
+
+module.exports = solveArrowFunctions;
---
+++ 02-Intermediate-Core/02-Arrow-Functions/02-Arrow-Functions.test.js
@@ -0,0 +1,30 @@
+const solveArrowFunctions = require('./02-Arrow-Functions.js');
+
+describe('Arrow-Functions', () => {
+  test('Happy Path: counts truthy and falsy values', () => {
+    expect(solveArrowFunctions([1, 0, 'JS', '', true, false])).toEqual({
+      topic: 'Arrow-Functions',
+      total: 6,
+      truthyCount: 3,
+      falsyCount: 3,
+    });
+  });
+
+  test('Edge Case: handles an empty input array', () => {
+    expect(solveArrowFunctions([])).toEqual({
+      topic: 'Arrow-Functions',
+      total: 0,
+      truthyCount: 0,
+      falsyCount: 0,
+    });
+  });
+
+  test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
+    expect(solveArrowFunctions([NaN, -0, '0'])).toEqual({
+      topic: 'Arrow-Functions',
+      total: 3,
+      truthyCount: 1,
+      falsyCount: 2,
+    });
+  });
+});
