2f7eee800beaefb8e61f356601243975018180c8
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 05:23:57 +0200

    Critical fix: Restore student-implementation challenge to Easy-Fundamentals lessons
    All 8 lessons in Easy-Fundamentals now have empty stubs instead of completed implementations.
    Students must implement each function for tests to pass, restoring the self-learning challenge.
    
    Lessons converted to stubs:
    - 01-Variables (var, let, const - 3 lessons)
    - 02-Data-Types
    - 03-Operators
    - 04-Control-Flow
    - 05-Loops
    - 06-Functions-Basics
    - 07-Arrays-Basics
    - 08-Objects-Basics
    
    Next: Apply same pattern to remaining 7 tracks (Intermediate through Engine-Internals)

--- 01-Easy-Fundamentals/01-Variables/01-var.js
+++ 01-Easy-Fundamentals/01-Variables/01-var.js
@@ -7,24 +7,7 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveVar(values) {
-  if (!Array.isArray(values)) {
-    throw new TypeError('values must be an array');
-  }
-
-  let truthyCount = 0;
-
-  for (const value of values) {
-    if (Boolean(value)) {
-      truthyCount += 1;
-    }
-  }
-
-  return {
-    topic: 'var',
-    total: values.length,
-    truthyCount,
-    falsyCount: values.length - truthyCount,
-  };
+  // TODO: Implement your solution
 }
 
 module.exports = solveVar;
--- 01-Easy-Fundamentals/06-Functions-Basics/06-Functions-Basics.js
+++ 01-Easy-Fundamentals/06-Functions-Basics/06-Functions-Basics.js
@@ -7,24 +7,7 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveFunctionsBasics(values) {
-  if (!Array.isArray(values)) {
-    throw new TypeError('values must be an array');
-  }
-
-  let truthyCount = 0;
-
-  for (const value of values) {
-    if (Boolean(value)) {
-      truthyCount += 1;
-    }
-  }
-
-  return {
-    topic: 'Functions-Basics',
-    total: values.length,
-    truthyCount,
-    falsyCount: values.length - truthyCount,
-  };
+  // TODO: Implement your solution
 }
 
 module.exports = solveFunctionsBasics;
