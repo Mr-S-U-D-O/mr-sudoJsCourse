9b27aa060be54d743383c6c38efa6891af127367
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 03:47:41 +0200

    upgrade lesson quality: 01-Easy-Fundamentals/05-Loops

--- 01-Easy-Fundamentals/05-Loops/05-Loops.test.js
+++ 01-Easy-Fundamentals/05-Loops/05-Loops.test.js
@@ -20,11 +20,25 @@ describe('Loops', () => {
   });
 
   test('Trap: counts NaN and -0 as falsy while "0" is truthy', () => {
-    expect(solveLoops([NaN, -0, '0'])).toEqual({
+    expect(solveLoops([NaN, -0, "0"])).toEqual({
       topic: 'Loops',
       total: 3,
       truthyCount: 1,
       falsyCount: 2,
     });
+  });
+
+  test('Validation: throws when input is not an array', () => {
+    expect(() => solveLoops(null)).toThrow('values must be an array');
+    expect(() => solveLoops('not-an-array')).toThrow('values must be an array');
+  });
+
+  test('Safety: does not mutate the input array', () => {
+    const input = [1, 0, 'x'];
+    const clone = [...input];
+
+    solveLoops(input);
+
+    expect(input).toEqual(clone);
   });
 });
