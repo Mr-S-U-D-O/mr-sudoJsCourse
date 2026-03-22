b5a3bc8f9686cc0e1cdbd501ead2b038679e4b45
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 04:34:50 +0200

    Course build: 07-Functional-Architecture/07-Clean-Architecture/07-Clean-Architecture.js

--- 07-Functional-Architecture/07-Clean-Architecture/07-Clean-Architecture.js
+++ 07-Functional-Architecture/07-Clean-Architecture/07-Clean-Architecture.js
@@ -7,7 +7,24 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveCleanArchitecture(values) {
-  // TODO: Your code here
+  if (!Array.isArray(values)) {
+    throw new TypeError('values must be an array');
+  }
+
+  let truthyCount = 0;
+
+  for (const value of values) {
+    if (Boolean(value)) {
+      truthyCount += 1;
+    }
+  }
+
+  return {
+    topic: 'Clean-Architecture',
+    total: values.length,
+    truthyCount,
+    falsyCount: values.length - truthyCount,
+  };
 }
 
 module.exports = solveCleanArchitecture;
