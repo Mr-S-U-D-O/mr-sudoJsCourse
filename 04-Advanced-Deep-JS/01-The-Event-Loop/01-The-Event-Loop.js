9a8598ba6bf8f1eb1466b8d9fcf79aaec25efac3
Molele Kishoez <molelekishoez@gmail.com>
2026-03-22 04:34:41 +0200

    Course build: 04-Advanced-Deep-JS/01-The-Event-Loop/01-The-Event-Loop.js

--- 04-Advanced-Deep-JS/01-The-Event-Loop/01-The-Event-Loop.js
+++ 04-Advanced-Deep-JS/01-The-Event-Loop/01-The-Event-Loop.js
@@ -7,7 +7,24 @@
  * @throws {TypeError} When values is not an array.
  */
 function solveTheEventLoop(values) {
-  // TODO: Your code here
+  if (!Array.isArray(values)) {
+    throw new TypeError('values must be an array');
+  }
+
+  let truthyCount = 0;
+
+  for (const value of values) {
+    if (Boolean(value)) {
+      truthyCount += 1;
+    }
+  }
+
+  return {
+    topic: 'The-Event-Loop',
+    total: values.length,
+    truthyCount,
+    falsyCount: values.length - truthyCount,
+  };
 }
 
 module.exports = solveTheEventLoop;
